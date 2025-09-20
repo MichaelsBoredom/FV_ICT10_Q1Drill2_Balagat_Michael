a954226bd5498cbd79c7446788b3f5530ada4bb0
MichaelsBoredom <tryfrnz1@gmail.com>
2025-09-20 18:23:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,11 @@ from pyscript import display, document
 
 def generate_message(e): 
 
-    # value inputs
+    # Values in input fields
     name = document.getElementById("name").value
     age = document.getElementById("age").value
     school = document.getElementById("school").value
 
-    # Restrict age to numbers only
-    if not age.isdigit():
-        document.getElementById("output").innerHTML
-        display("Please enter a valid age (numbers only).", target="output")
     # Escape characters for \t (tab), \" (quote)
     notes = f"\t{name} is currently {age} years old and studies at \"{school}\".\n"
     notes += "\tThis information was gathered through input fields and displayed using\n"
